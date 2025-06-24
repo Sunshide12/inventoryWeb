2648a425b497f0f1b08d20036dc2d5cb7272b528
Steban Martínez <stebanbusiness@gmail.com>
2025-06-24 08:16:54 -0500

    Agregando los forms del login y register

--- javascript/main.js
+++ javascript/main.js
@@ -11,3 +11,18 @@ fetch("http://localhost/inventory/php/get-products.php")
   .catch(error => {
     console.error('Hubo un problema con la petición fetch:', error);
   });
+
+
+  $(document).ready(function() {
+  $('#show-register').click(function(e) {
+    e.preventDefault();
+    $('.login-form').hide();
+    $('.register-form').show();
+  });
+
+  $('#show-login').click(function(e) {
+    e.preventDefault();
+    $('.register-form').hide();
+    $('.login-form').show();
+  });
+});
